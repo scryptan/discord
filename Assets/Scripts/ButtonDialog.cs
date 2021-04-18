eb6faf551456c1610a32658192f64ac4d7180468
leoska <leo77551@gmail.com>
2021-04-18 19:56:43 +0300

    WIP

---
+++ Assets/Scripts/ButtonDialog.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonDialog : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Image _imageRender = null;
+    private bool _mouseHover = false;
+
+    public Sprite spriteCommon = null;
+    public Sprite spriteHover = null;
+    public Sprite spritePressed = null;
+
+    private void Awake()
+    {
+        _imageRender = GetComponent<Image>();
+        _mouseHover = false;
+    }
+
+    private void Update()
+    {
+        if (!_mouseHover) return;
+        
+        if (Input.GetMouseButton(0))
+            _imageRender.sprite = spritePressed;
+            
+        else if (Input.GetMouseButtonUp(0))
+            Process();
+    }
+    
+    private void Process()
+    {
+        _imageRender.sprite = _mouseHover ? spriteHover : spriteCommon;
+        
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _imageRender.sprite = spriteHover;
+        _mouseHover = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _imageRender.sprite = spriteCommon;
+        _mouseHover = false;
+    }
+}
--- Assets/Scripts/GameDialog.cs
+++ Assets/Scripts/GameDialog.cs
@@ -1,11 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
 
 [ExecuteAlways]
 public class GameDialog : MonoBehaviour
 {
+    [Header("Canvas Game Object")]
     public GameObject canvasDialog = null;
+
+    public GameObject commonDialog = null;
+    public GameObject failedDialog = null;
+
+    [Header("Dialogs")] 
+    public string startGuyText = "";
+    public DialogCommon[] dialogCommon;
+    public SpriteAtlas girlDialog = null;
+    
+    
     // Start is called before the first frame update
     private void Start()
     {
@@ -14,12 +27,12 @@ public class GameDialog : MonoBehaviour
     
     private void OnEnable()
     {
-        canvasDialog?.SetActive(true);
+        if (!(canvasDialog is null)) canvasDialog?.SetActive(true);
     }
 
     private void OnDisable()
     {
-        canvasDialog?.SetActive(false);
+        if (!(canvasDialog is null)) canvasDialog?.SetActive(false);
     }
 
     // Update is called once per frame
@@ -27,4 +40,35 @@ public class GameDialog : MonoBehaviour
     {
         
     }
+
+    [Serializable]
+    public struct DialogCommon
+    {
+        [Multiline(3)]
+        public string textGirl;
+        public GirlEmotion girlEmotion;
+
+        public TextGuy[] textGuy;
+    }
+
+    [Serializable]
+    public class TextGuy
+    {
+        [Multiline(3)]
+        public string text = "";
+        public bool badText = false;
+
+        public string failText = "";
+    }
+
+    public enum GirlEmotion
+    {
+        common,
+        questionable,
+        hey,
+        angry,
+        wrong,
+        cute,
+    }
+    
 }
